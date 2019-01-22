bf6bf76ca9e82b79ecafe8c0901811acfb27ae74
nimaphoenix <46718273+nimapheonix@users.noreply.github.com>
2019-01-22 15:53:07 +0330

    product service

--- src/app/Products/product.service.ts
+++ src/app/Products/product.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-
+import { Product } from './product';
+import { products } from './products';
+import { Observable,of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
   constructor() { }
+
+  getProducts(): Observable<Product[]> {
+
+    return of(products);
+   }
+
 }
